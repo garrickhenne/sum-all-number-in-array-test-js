dfcada6d42bed5f3f830fa3d695a944b0703d835
Garrick Henne <garrickhenne@outlook.com>
2023-09-21 17:53:59 -0700

    Implemented sumItems recursively

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  // Sum all the numbers in the array.
 
+  // Base case.
+  if (array.length === 0) {
+    return 0;
+  }
+
+  // Recursive case
+  let sum = 0;
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+
+  return sum;
+};
+
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+// [1, 2, 3, 4, 5] => 15
 module.exports = sumItems;
